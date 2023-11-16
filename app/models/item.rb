7194929d864448935d7936ca997667b78e8a3ccd
fucchihub <144200649+fucchihub@users.noreply.github.com>
2023-11-16 20:38:18 +0000

    admin/items 全体が一応完成

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,18 @@ class Item < ApplicationRecord
   validates :introduction, presence: true
   validates :before_tax, presence: true
   validates :image, presence: true
+
+# このメソッドがないと販売ステータスが日本語ではなくtrue, falseと表示される。
+  def sale_status(item)
+    if item.is_sale
+      '販売中'
+    else
+      '販売停止中'
+    end
+  end
+
+# 税込み価格を表示するためのメソッド。
+  def after_tax
+    (self.before_tax * 1.10).round
+  end
 end
