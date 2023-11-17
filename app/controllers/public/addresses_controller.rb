e816c06c14be101ed9f36657e9c5cd6cbf806454
Junpei1121 <momooooonnn@gmail.com>
2023-11-17 06:12:16 +0000

    途中

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,31 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address=Address.new
+    @addresses=Address.all
   end
-
+  
+  def create
+    @address=Address.new(address_params)
+    if @address.save
+      redirect_to :index
+    else
+      render :index
+    end
+  end
+  
+  def update
+  end
+  
   def edit
   end
+  
+  def destroy
+  end
+  
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :post_code, :address)
+  end
+  
 end
