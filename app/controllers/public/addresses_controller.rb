4d71a06829b2f36df6e12cf767be9be91ff813d8
Junpei1121 <momooooonnn@gmail.com>
2023-11-17 07:53:20 +0000

    public-addresses

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -6,26 +6,38 @@ class Public::AddressesController < ApplicationController
   
   def create
     @address=Address.new(address_params)
+    @address.customer_id=current_customer.id
     if @address.save
-      redirect_to :index
+      redirect_to addresses_path
     else
       render :index
     end
   end
   
-  def update
-  end
-  
   def edit
+    @address=Address.find(params[:id])
   end
   
+  def update
+    @address=Address.find(params[:id])
+    if @address.update(address_params)
+      flash[:notice] ="更新できました"
+      redirect_to addresses_path
+    else
+      render :edit
+    end
+  end
+
   def destroy
+    address=Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
   end
   
   private
 
   def address_params
-    params.require(:address).permit(:name, :post_code, :address)
+    params.require(:address).permit(:name, :post_cord, :address)
   end
   
 end
