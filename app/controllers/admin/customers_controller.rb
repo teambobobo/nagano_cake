cba850e8218a1d8c4cc5b10bc7682c3ab1ed7884
Junpei1121 <momooooonnn@gmail.com>
2023-11-17 13:31:02 +0000

    admin-customers80%

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,37 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers=Customer.all
   end
 
   def show
+    @customer=Customer.find(params[:id])
   end
 
   def edit
+    @customer=Customer.find(params[:id])
   end
+  
+  def update
+    @customer=Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] ="更新成功"
+      redirect_to admin_customer_path(@customer)
+    else
+      render :edit
+    end
+  end
+  
+   private
+
+  def customer_params
+    params.require(:customer).permit(:last_name,
+                                     :first_name,
+                                     :kana_last_name,
+                                     :kana_first_name,
+                                     :phone_number,
+                                     :post_cord,
+                                     :address,
+                                     :email)
+  end
+  
 end
