434a6800155865f52514cf1cfb58c1a931eb11ba
HasegawaYuuki <hasegawa990212@gmail.com>
2023-11-18 13:03:27 +0000

    新着商品大体完成

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,8 @@
 class Public::HomesController < ApplicationController
   def top
     items = Item.all
-    @new_items = item
+    @new_items = items
+    @new_items = items.all.order(created_at: :desc).limit(4)
   end
 
   def about
