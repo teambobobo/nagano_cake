c79bcdaa6b915d269e2a4d408a6409fd6b784e90
Hayasaka <146614468+kuromame18@users.noreply.github.com>
2023-11-18 09:09:17 +0000

    public-cartitem-addition

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,33 @@ Admin.create!(
    email: 'admin@admin',
    password: 'adminadmin'
 )
+
+   15.times do |n|
+      Customer.create!(
+         last_name: "試験#{n + 1}",
+         first_name: "太郎#{n + 1}",
+         kana_last_name: "シケン#{n + 1}",
+         kana_first_name: "タロウ#{n + 1}",
+         phone_number: "00000#{n + 1}",
+         post_cord: "000000#{n + 1}",
+         address: "試験町#{n + 1}",
+         email: "test#{n + 1}@gmail.com",
+         password: "123456"
+         )
+      end
+
+   Genre.create(name: "ケーキ")
+   Genre.create(name: "プリン")
+   Genre.create(name: "焼き菓子")
+   Genre.create(name: "キャンディ")
+
+   10.times do |n|
+      item=Item.create!(
+         genre_id: 1,
+         name: "商品#{n + 1}",
+         introduction: "商品説明#{n + 1}",
+         before_tax: 2000
+         )
+      # item.image.attach(io: File.open(Rails.root.join( 'app/assets/images/no_image.jpg')),
+         # filename: 'no_image.jpg')
+      end
