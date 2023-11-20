02e56362dff1f6441214cab0a86847299df5a406
fucchihub <144200649+fucchihub@users.noreply.github.com>
2023-11-20 10:39:17 +0000

    public/items authenticateの記述削除

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,5 +1,4 @@
 class Public::ItemsController < ApplicationController
-  before_action :authenticate_customer!
   def index
     @items = Item.where(is_sale: true).page(params[:page]).per(10).order(:id)
   end
