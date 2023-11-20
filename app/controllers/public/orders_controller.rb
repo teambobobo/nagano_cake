b69a158924376d20b857ba1aa537577ce9f89b3a
HasegawaYuuki <hasegawa990212@gmail.com>
2023-11-20 09:41:07 +0000

    注文情報確認画面　注文履歴画面レイアウト以外多分できた

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,7 +9,6 @@ class Public::OrdersController < ApplicationController
     @postage = 800
     @payment_method = params[:order][:payment_method]
     @address_option = params[:order][:address_option]
-    puts @address_option
     if @address_option == "1"
       @order.address_name = current_customer.last_name + current_customer.first_name
       @order.post_code = current_customer.post_cord
@@ -21,16 +20,50 @@ class Public::OrdersController < ApplicationController
     else
       if params[:order][:post_code] == "" || params[:order][:address] == "" || params[:order][:address_name] == ""
         render :new
+      else
+        @order.address_name = params[:order][:name]
+        @order.post_code = params[:order][:post_code]
+        @order.address = params[:order][:address]
       end
     end
-    puts @address_option
-    p @order
+    @cart_items = current_customer.cart_items.all
+    @total = 0
+    @order.customer_id = current_customer.id
+  end
+
+  def create
+    @order = Order.new(order_params)
+    #ordersに書かれているものを入れていく（客の情報を入れていく）
+    #@order.saveしても問題ない
+    if @order.save
+      cart_items = current_customer.cart_items.all#一個ずつ取り出したい
+    #繰り返しの構文を書く
+      cart_items.each do |cart_item|
+    #保存用のかご記入
+        @order_details = OrderDetail.new
+    #order_detailsに書かれているものをカゴに入れていく
+        @order_details.item_id = cart_item.item.id#何の商品なのか判断
+        @order_details.amount = cart_item.amount#何個注文したか
+        @order_details.order_id = @order.id
+        #@order_details.status = cart_item.status
+        @order_details.after_tax = cart_item.item.after_tax
+    #order_detailsを保存
+        @order_details.save
+      end
+    #カートアイテムを空にする
+      cart_items.destroy_all
+      redirect_to complete_path
+    else
+      render :new
+    end
   end
 
   def complete
   end
 
   def index
+    @order = Order.all
+    @order_details = OrderDetail.all
   end
 
   def show
@@ -39,6 +72,14 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:payment_method, :address_name, :post_code, :address)
+    params.require(:order).permit(:payment_method,
+                                  :address_name,
+                                  :post_code,
+                                  :address,
+                                  :total_price,
+                                  :customer_id,
+                                  :postage)
   end
+
+
 end
