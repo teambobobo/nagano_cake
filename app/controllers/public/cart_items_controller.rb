7b996f6233df54db66f81865cfc70fb5d93fb18e
Hayasaka <146614468+kuromame18@users.noreply.github.com>
2023-11-21 05:13:16 +0000

    cart_item update

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,10 +6,17 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    @cart_item.save
-    redirect_to cart_items_path
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      @cart_item.amount = @cart_item.amount.to_i + params[:cart_item][:amount].to_i
+      @cart_item.save
+      redirect_to cart_items_path
+    else
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      @cart_item.save
+      redirect_to cart_items_path
+    end
   end
 
   def update
