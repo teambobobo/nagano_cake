62b5c0d98cddbb171acfbc1e2ab1541ce49f6ce1
fucchihub <144200649+fucchihub@users.noreply.github.com>
2023-11-22 04:37:57 +0000

    やりかけ

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -11,7 +11,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to admin_items_path
+      redirect_to admin_items_path, notice: "商品を登録しました"
     else
       render :new
     end
@@ -28,7 +28,7 @@ class Admin::ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to admin_items_path
+      redirect_to admin_items_path, notice: "商品を更新しました"
     else
       render :edit
     end
