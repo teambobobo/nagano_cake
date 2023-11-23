69e793f567962f847958540e922e9e068713a4f5
Junpei1121 <momooooonnn@gmail.com>
2023-11-23 07:16:29 +0000

    public_addressnoレイアウトとフラッシュメッセージ実装

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -3,26 +3,27 @@ class Public::AddressesController < ApplicationController
     @address=Address.new
     @addresses=Address.all
   end
-  
+
   def create
     @address=Address.new(address_params)
     @address.customer_id=current_customer.id
     if @address.save
+      flash[:public_address] ="登録できました"
       redirect_to addresses_path
     else
       @addresses = Address.all
       render :index
     end
   end
-  
+
   def edit
     @address=Address.find(params[:id])
   end
-  
+
   def update
     @address=Address.find(params[:id])
     if @address.update(address_params)
-      flash[:notice] ="更新できました"
+      flash[:public_address] ="更新できました"
       redirect_to addresses_path
     else
       render :edit
@@ -32,13 +33,14 @@ class Public::AddressesController < ApplicationController
   def destroy
     address=Address.find(params[:id])
     address.destroy
+    flash[:public_address] ="削除しました"
     redirect_to addresses_path
   end
-  
+
   private
 
   def address_params
     params.require(:address).permit(:name, :post_cord, :address)
   end
-  
+
 end
